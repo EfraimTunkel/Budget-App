714902a422767d415a53a69b43a34bd82dfe39d0
efraimt13 <efraimt13@gmail.com>
2025-01-01 21:21:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -18,15 +18,14 @@ import {
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDuDrJSgnGvkDHCdIBq98m2zRGLvwRgbYs",
+  authDomain: "budgetapp-5d500.firebaseapp.com",
+  projectId: "budgetapp-5d500",
+  storageBucket: "budgetapp-5d500.firebasestorage.app",
+  messagingSenderId: "31114956560",
+  appId: "1:31114956560:web:1cbf62fbeaa484114ddf95",
+  measurementId: "G-X9P0P9FC43",
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
